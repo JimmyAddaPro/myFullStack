6c5634f32dbec1629d1d575487626f0d462e3359
Jimmy Adda <contact@jimmyadda.com>
2023-01-26 14:54:32 +0100

    cours JS 22.01.2023

---
+++ 02-javascript/01-explication-code/22.01.2023/02-ternaire-découverte/js/main.js
@@ -0,0 +1,17 @@
+
+var z = "";
+var x = prompt("Entre un chiffre");
+
+//Exemple 1 : Condition, écriture courante
+if(x > 5){
+    z = "Hello";
+} else {
+    z = "Good bye";
+} 
+// Exemple 2 : Condition, écriture ternaire. (plus sophistiquée)
+
+var z = x > 5 ? "Hello" : "Goodbye"; 
+
+//alert(z);
+
+
