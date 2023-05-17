80de6b4937bc6d7d716be6a18fed80b111b035eb
Jimmy Adda <contact@jimmyadda.com>
2023-05-17 13:38:06 +0200

    Object Date decouverte - 12.02/2023

---
+++ 02-javascript/01-explication-code/02.2023/12.02.2023/02-object-date-champ-input/js/main.js
@@ -0,0 +1,133 @@
+// Reprendre 12/02/2023 - 00:48:00
+const Main = document.getElementById("main");
+var btn = document.createElement("button");
+btn.innerText = "Get Time my Friend";
+
+var myInput = document.createElement("input");
+
+myInput.type = "date";
+myInput.className = "my-input";
+myInput.placeholder = "Date of Birth";
+
+Main.append(btn, myInput);
+
+btn.addEventListener("click", function(){
+  var d = new Date(myInput.value);
+  var divDate = document.createElement("div");
+  divDate.className = "date";
+  divDate.innerHTML =  "Date : "               + d;
+  divDate.innerHTML += "<hr style=\"border: 2px solid red\">";
+  divDate.innerHTML +=  "Date : "               + d.toLocaleDateString(); // 
+  Main.append(divDate);
+});
+/*
+    Objectif  :  
+       énoncé    :  Découverte de l'Objet DATE
+        1.....   
+        2.....   
+        ----------------------------------------------------------------------------------------
+        Evolution : 
+        1. grande étape 
+            - petite étape.
+            - petite étape.
+        2.etc.   
+        FIN
+        --
+*/
+/*
+    Methodes/Fonctions  :
+    
+    Element.remove()               La Element.remove()méthode supprime l'élément du DOM.
+    --
+    String.prototype.replaceAll() Permet de remplacer des carractères dans un String.
+    ->  retourne -> -------------- Nouveau String
+    ->  arg      -> -------------- Carractère recherché
+    ->  arg 2    -> -------------- Carractère de remplacement
+    ->  exemple  -> -------------- "aabbcc".replaceAll("b", ".");   // 'aa..cc'
+    --
+    String.prototype.toLowerCase() 
+    ->  retourne -> -------------- renvoie la valeur de la chaîne d'appel convertie en minuscules.
+    --
+    EventTarget.addEventListener() Configure une fonction qui sera appelée chaque fois que l'événement spécifié est livré à la cible.
+    ->  retourne -> -------------- ?
+    ->  arg      -> -------------- évènement visé pour que la fonction soit appelée
+    ->  arg 2    -> -------------- fonction appelé à chaque fois que l'élément est visé
+    ->  exemple  -> -------------- emailInput.addEnventListener("input", checkEmail)
+    ->  /!\      -> -------------- L'argument 1 attend le type d'événement. Cela peut prêter à confusion quand le nom de l'évènement est un nom de champ(ex : input) ce qui veut dire : quand l'utilisateur écrit dans le champs ou le modifie.Quand l'évènement est click c'est plus clair, mais dans les deux cas c'est un évènement.Voici à quoi il faut faire référence : https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+    ->  /!\      -> -------------- L'argument 2 attend la fonction sans les parenthèses. La fonction sera alors déclanchée qu'au moment ou le Listener renvoie True.
+    --          
+    document.getElementById() ---- Permet de sélectionner un élément par son ID
+    ->  retourne -> -------------- Un object
+    ->  exemple  -> -------------- var objMain = document.getElementById("main");
+    --                           
+    createElement() -------------- Permet de créer un élément HTML
+    ->  arg      -> -------------- tagName/nom de l'élément
+    ->  retourne -> -------------- Un nouvel élément.
+    ->  exemple  -> -------------- var email = document.createElement("input");
+    --     
+    Element.append() -------------- Ajoute du contenu à l'intérieur et à la fin d'un élément.
+    ->  arg      ->  -------------- Elément à ajouter
+    ->  retourne ->  -------------- ?
+    ->  exemple  ->  -------------- emailLabel.append(emailInput);
+    --     
+    Element.prepend() ------------- Ajoute du contenu à l'intérieur et au début d'un élément.
+    ->  arg      ->  -------------- Elément à ajouter
+    ->  retourne ->  -------------- ?
+    ->  exemple  ->  -------------- emailLabel.prepend(emailInput); 
+    --    
+    NomDeLaMethode() -------------- Utilité principale
+    ->  arg      ->  --------------
+    ->  retourne ->  --------------
+    ->  exemple  ->  --------------
+*/
+/*
+    Propriétés/Variables  :  
+    HTMLElement.innerText --------- Renseigne du contenu dans un élément HTML
+    ->  exemple  -> --------------- emailLabel.innerText = "Enter your email";
+    --  
+    HTMLElement.innerHTML --------- Renseigne du contenu HTML dans un élément HTML
+    ->  exemple  -> --------------- var emailDiv = `<div></div>`;
+    ->           -> --------------- emailDiv.innerHtml = `<strong>Texte ...</strong>`;
+    -> /!\       -> --------------- innerHTML n'attends que du String qui sera transformé par le navigateur en HTML. On ne peut donc pas lui affecter comme valeur un Object. 
+    -> à noter   -> --------------- Il est préférable de créer des Objects et ensuite de travailler avec eux comme on veut, plutôt que de faire un bloc d'HTML qu'on ne pourra pas modifier. 
+    --  
+    NomDeLaPropriété      : Utilité principale
+    ->  exemple  ->
+
+Instructions  :  
+return ------------------------ L'instruction return met fin à l'exécution d'une fonction et définit une valeur à renvoyer à la fonction appelante. mais ici nous l'utilisons pour interompre un fonction.
+-> Une fonction s'arrête immédiatement à l'instant où l'instruction return est traitée.
+->  exemple  -> 
+/* -------------------------------------------------------
+function compteur() {
+    for (var compte = 1; ; compte++) {  // boucle infinie
+      console.log(compte + "A"); // jusqu'à 5
+        if (compte === 5) {
+          return;
+        }
+        console.log(compte + "B");  // jusqu'à 4
+      }
+    console.log(compte + "C");  // cette instruction n'est jamais utilisée
+  }
+  
+  compteur();
+  
+  // Résultat dans la console :
+  // 1A
+  // 1B
+  // 2A
+  // 2B
+  // 3A
+  // 3B
+  // 4A
+  // 4B
+  // 5A
+------------------------------------------------------- */
+/*
+    Bonnes pratiques : 
+    Pour une application ou même de manière générale, le main n'est utilisé qu'une seule fois.            -
+    --> Amélioration 00 
+        /P\ ... ?
+        /S\ ...
+        /!\ ...
+*/
